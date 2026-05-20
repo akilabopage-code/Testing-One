4a01b3a60febc5da4b80efb7ff3eb3161d494f52
akilabopage-code <akila.bopage@gmail.com>
2026-05-20 16:46:37 +0530

    new to chek pull request

--- Printing_numbers.py
+++ Printing_numbers.py
@@ -3,12 +3,9 @@ y=0
 while(x>0):
  print("Learning python")
  x-=1
-<<<<<<< Updated upstream
- print("Learning python is fun")
-=======
  print("Blastoff!")
 
 f=50
 if(f==50):
  print("f is 50")
->>>>>>> Stashed changes
+ print("hhh")
